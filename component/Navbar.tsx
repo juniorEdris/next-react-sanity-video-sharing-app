7315d2566cab20267f48e34d5c0e54afdc4929a7
Eftekar Raghib <70286969+juniorEdris@users.noreply.github.com>
2022-10-03 01:01:11 +0600

    updated navbar

---
+++ component/Navbar.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { IoMdAdd } from "react-icons/io";
+import Logo from "../utils/tiktik-logo.png";
+
+const Navbar: NextPage = () => {
+  return (
+    <div className="w-full flex justify-between item-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[130px] h-[38px]">
+          <Image
+            src={Logo}
+            alt="logo"
+            className="cursor-pointer"
+            layout="responsive"
+          />
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ component/Sidebar.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Sidebar: NextPage = () => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
